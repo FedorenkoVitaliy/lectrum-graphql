f78cddb01ff87bf1d86969941a9b69a908c63731
Виталий Федоренко <fedvetal2012@gmail.com>
2020-09-22 02:03:48 +0300

    feat: use custom hook for query

--- src/App/index.js
+++ src/App/index.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks'
 
+//Components
+import { Pet } from './bus/pet';
+
 //Other
-import { client } from './init/client'
+import { client } from './init/client';
 
 export const App = () => {
   return(
     <ApolloProvider client={client}>
-      <>
-        <p>Welcome</p>
-      </>
+      <Pet/>
     </ApolloProvider>
   )
 };

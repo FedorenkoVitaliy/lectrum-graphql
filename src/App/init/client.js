23ac1986046b9908f4f3fbd04148c1a976cff000
Виталий Федоренко <fedvetal2012@gmail.com>
2020-10-07 02:05:58 +0300

    feat: use Apollo context

--- src/App/init/client.js
+++ src/App/init/client.js
@@ -2,15 +2,28 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
 
 //GraphQL Server
 const uri = 'https://funded-pet-library.moonhighway.com/';
-const link = createHttpLink({uri});
+const httpLink = createHttpLink({uri});
 
-//cache initialization
+//Auth
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}`: '',
+    }
+  }
+})
+
+//Cache initialization
 const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
   cache,
-  link
+  link: authLink.concat(httpLink)
 })
